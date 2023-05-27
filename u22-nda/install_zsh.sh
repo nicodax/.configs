c34bc36a60d670c0d7a1e3dd9e29d4667c91c2a5
nicodax <nicodax@proton.me>
2023-05-27 16:14:36 +0200

    fix: remove powerlevel10k

--- u22-nda/install_zsh.sh
+++ u22-nda/install_zsh.sh
@@ -13,13 +13,12 @@ git clone $ZSH_AUTOSUGGESTIONS_GIT ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugin
 git clone $ZSH_SYNTAX_HIGHLIGHTING_GIT ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 ## INSTALL ASDF & NODE & RUBY & RAILS & PYTHON
 git clone $ASDF_GIT $HOME/.asdf
-## INSTALL POWERLEVEL10K
+## INSTALL MESLO FONTS
 mkdir -p $USER_FONT_PATH
 wget $MESLO_LGS_NF_REGULAR_URL -O $USER_FONT_PATH/MesloLGS-NF-Regular.ttf
 wget $MESLO_LGS_NF_BOLD_URL -O $USER_FONT_PATH/MesloLGS-NF-Bold.ttf
 wget $MESLO_LGS_NF_ITALIC_URL -O $USER_FONT_PATH/MesloLGS-NF-Italic.ttf
 wget $MESLO_LGS_NF_BOLD_ITALIC_URL -O $USER_FONT_PATH/MesloLGS-NF-BoldItalic.ttf
-git clone --depth=1 $POWERLEVEL10K_GIT ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 ## ZSHRC
 rm $HOME/.zshrc
 cp src/.zshrc $HOME/.zshrc
